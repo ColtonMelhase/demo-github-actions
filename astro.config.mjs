8462c75e2d2ee3ebe2e98b22ae5fe5aa118de007
Colton Melhase <coltonmelhase12@gmail.com>
2025-05-08 22:04:13 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import prefetch from "@astrojs/prefetch";
 
 export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
-  site: "https://ColtonMelhase.github.io",
+  site: "https://ColtonMelhase.github.io/demo-github-actions",
   base: "/demo-github-actions",
 });
